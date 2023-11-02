eee5f12022a773abf731a31335d310be49169b1e
Aura Román <kyradiscord@gmail.com>
2023-11-02 15:16:00 +0100

    refactor: better syntax

--- Assets/Scripts/Scene Mechanics/Goal.cs
+++ Assets/Scripts/Scene Mechanics/Goal.cs
@@ -4,15 +4,26 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Goal : MonoBehaviour
 {
-    private int Level { get; set; } = -1;
+    private int NextScene { get; set; } = -1;
+    private static int MainMenuScene { get; set; } = -1;
 
     private void Awake()
     {
+        // Get the build index for the main menu scene if it hasn't been loaded yet:
+        if (MainMenuScene == -1)
+        {
+            MainMenuScene = SceneUtility.GetBuildIndexByScenePath("Assets/Scenes/Main Menu.unity");
+        }
+
         // Get current scene name and verify it's formatted correctly with the format 'LevelN', where N is an integer:
         var sceneName = SceneManager.GetActiveScene().name;
         if (sceneName.Length > 5 && sceneName.StartsWith("Level") && int.TryParse(sceneName[5..], out var level))
         {
-            Level = level;
+            NextScene = SceneUtility.GetBuildIndexByScenePath($"Assets/Scenes/Level{level + 1}.unity") switch
+            {
+                -1 => MainMenuScene,
+                var v => v
+            };
         }
         else
         {
@@ -23,12 +34,9 @@ public class Goal : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (Level == -1 || !collision.CompareTag("Player")) return;
+        if (!collision.CompareTag("Player")) return;
 
-        var index = SceneUtility.GetBuildIndexByScenePath($"Assets/Scenes/Level{Level + 1}.unity");
-        
         // If the next level doesn't exist, go back to the main menu:
-        if (index == -1) SceneManager.LoadScene("Scenes/Main Menu");
-        else SceneManager.LoadScene(index);
+        SceneManager.LoadScene(NextScene);
     }
 }
