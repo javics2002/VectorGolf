4fe04574c1bd1a3f574f397694472c5bf42799ba
Aura Román <kyradiscord@gmail.com>
2023-11-02 14:56:48 +0100

    fix: fixed automatic scene loader

--- Assets/Scripts/Scene Mechanics/Goal.cs
+++ Assets/Scripts/Scene Mechanics/Goal.cs
@@ -4,38 +4,31 @@ using UnityEngine.SceneManagement;
 [RequireComponent(typeof(BoxCollider2D))]
 public class Goal : MonoBehaviour
 {
-    private Scene? NextScene { get; set; }
+    private int Level { get; set; } = -1;
 
     private void Awake()
     {
         // Get current scene name and verify it's formatted correctly with the format 'LevelN', where N is an integer:
         var sceneName = SceneManager.GetActiveScene().name;
-        if (sceneName.Length <= 5 || !sceneName.StartsWith("Level") || !int.TryParse(sceneName[5..], out var level))
+        if (sceneName.Length > 5 && sceneName.StartsWith("Level") && int.TryParse(sceneName[5..], out var level))
         {
-            Debug.LogError("Current scene name is not formatted correctly, it must be named with the format 'LevelN', where N is an integer!'");
-            enabled = false;
-            return;
-        }
-
-        var nextSceneName = $"Level{level + 1}";
-        var scene = SceneManager.GetSceneByName(nextSceneName);
-        if (scene.IsValid())
-        {
-            NextScene = scene;
+            Level = level;
         }
         else
         {
-            Debug.Log($"Could not find scene {nextSceneName}, using main menu as next scene.");
-            NextScene = SceneManager.GetSceneByName("Main Menu");
+            Debug.LogError("Current scene name is not formatted correctly, it must be named with the format 'LevelN', where N is an integer!'");
+            enabled = false;
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Goal reached!");
-        if (collision.CompareTag("Player") && NextScene.HasValue)
-        {
-            SceneManager.LoadScene(NextScene.Value.name);
-        }
+        if (Level == -1 || !collision.CompareTag("Player")) return;
+
+        var index = SceneUtility.GetBuildIndexByScenePath($"Assets/Scenes/Level{Level + 1}.unity");
+        
+        // If the next level doesn't exist, go back to the main menu:
+        if (index == -1) SceneManager.LoadScene("Scenes/Main Menu");
+        else SceneManager.LoadScene(index);
     }
 }
