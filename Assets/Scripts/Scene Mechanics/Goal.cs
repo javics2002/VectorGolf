6ba74be70915f175ee4bc6eb382abd6e7ec0c9ce
Aura Román <kyradiscord@gmail.com>
2023-11-14 15:32:23 +0100

    feat: add goal sprites and automatic colour setting

--- Assets/Scripts/Scene Mechanics/Goal.cs
+++ Assets/Scripts/Scene Mechanics/Goal.cs
@@ -1,11 +1,27 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.U2D;
+using UnityEngine.U2D.Animation;
 
+[RequireComponent(typeof(SpriteRenderer))]
 [RequireComponent(typeof(BoxCollider2D))]
 public class Goal : MonoBehaviour
 {
+    [SerializeField]
+    private SpriteAtlas Atlas;
+
+    /// <summary>
+    /// The level this goal is for, calculated from the scene name.
+    /// </summary>
+    private int Level { get; set; } = -1;
     private int NextScene { get; set; } = -1;
     private static int MainMenuScene { get; set; } = -1;
+    
+    private const string AtlasFlagDefault = "game-flag-default";
+    private const string AtlasFlagRed = "game-flag-red";
+    private const string AtlasFlagGold = "game-flag-gold";
+    private const string AtlasFlagPlatinum = "game-flag-platinum";
 
     private void Awake()
     {
@@ -19,6 +35,7 @@ public class Goal : MonoBehaviour
         var sceneName = SceneManager.GetActiveScene().name;
         if (sceneName.Length > 5 && sceneName.StartsWith("Level") && int.TryParse(sceneName[5..], out var level))
         {
+            Level = level;
             NextScene = SceneUtility.GetBuildIndexByScenePath($"Assets/Scenes/Level{level + 1}.unity") switch
             {
                 -1 => MainMenuScene,
@@ -32,11 +49,31 @@ public class Goal : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        // Set the sprite to the correct flag sprite from the atlas:
+        GetComponent<SpriteRenderer>().sprite = Atlas.GetSprite(GetGoalSpriteName());
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Player")) return;
 
         // If the next level doesn't exist, go back to the main menu:
         SceneManager.LoadScene(NextScene);
+    }
+
+    private string GetGoalSpriteName()
+    {
+        // If the current level is 0 or greater than the number of levels, return the default flag sprite:
+        if (Level < 0 || Level >= GameManager.Instance.levelCompletion.Length) return AtlasFlagDefault;
+
+        return GameManager.Instance.levelCompletion[Level] switch
+        {
+            GameManager.LevelCompletion.HoleInOne => AtlasFlagPlatinum,
+            GameManager.LevelCompletion.Par => AtlasFlagGold,
+            GameManager.LevelCompletion.Completed => AtlasFlagRed,
+            _ => AtlasFlagDefault
+        };
     }
 }
