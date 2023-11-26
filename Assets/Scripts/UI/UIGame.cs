16be336f99b28447d0cbe31c525df036508f361a
Aura Román <kyradiscord@gmail.com>
2023-11-26 19:52:31 +0100

    feat: add game UI buttons

---
+++ Assets/Scripts/UI/UIGame.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(UIDocument))]
+public class UIGame : MonoBehaviour
+{
+	public void Start()
+	{
+		var root = GetComponent<UIDocument>().rootVisualElement;
+
+		root.Q<Button>("button-exit").clicked += OnButtonExit;
+		root.Q<Button>("button-settings").clicked += OnButtonSettings;
+		root.Q<Button>("button-restart").clicked += OnButtonRestart;
+	}
+
+	private void OnButtonExit()
+	{
+		SceneManager.LoadScene("Main Menu");
+	}
+
+	private void OnButtonSettings()
+	{
+		SceneManager.LoadScene("Settings", LoadSceneMode.Additive);
+	}
+
+	private void OnButtonRestart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+}
