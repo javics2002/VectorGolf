fd067c33718a204dcb0fe86385a379ccfbfdb96f
Aura Román <kyradiscord@gmail.com>
2023-11-27 23:58:17 +0100

    refactor: deduplicate code

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -1,27 +1,45 @@
-﻿using System.Xml.Linq;
-
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 [RequireComponent(typeof(UIDocument))]
 public class UIGame : MonoBehaviour
 {
-	private VisualElement root;
+	private Button _buttonExit;
+	private Button _buttonSettings;
+	private Button _buttonRestart;
 
 	public void Start()
 	{
-		root = GetComponent<UIDocument>().rootVisualElement;
+		var root = GetComponent<UIDocument>().rootVisualElement;
 
-		root.Q<Button>("button-exit").clicked += OnButtonExit;
-		root.Q<Button>("button-settings").clicked += OnButtonSettings;
-		root.Q<Button>("button-restart").clicked += OnButtonRestart;
+		(_buttonExit = root.Q<Button>("button-exit")).clicked += OnButtonExit;
+		(_buttonSettings = root.Q<Button>("button-settings")).clicked += OnButtonSettings;
+		(_buttonRestart = root.Q<Button>("button-restart")).clicked += OnButtonRestart;
 
 		SceneManager.sceneUnloaded += OnSceneUnloaded;
 	}
 
-	private void OnDestroy() {
+	private void OnDestroy()
+	{
 		SceneManager.sceneUnloaded -= OnSceneUnloaded;
+	}
+
+	private void OnSceneUnloaded(Scene scene)
+	{
+		EnableUI(scene.name == "Settings");
+	}
+
+	private void EnableUI(bool value)
+	{
+		_buttonExit.SetEnabled(value);
+		_buttonSettings.SetEnabled(value);
+		_buttonRestart.SetEnabled(value);
+
+		foreach (var item in FindObjectsOfType<DraggableItem>())
+		{
+			item.canDrag = value;
+		}
 	}
 
 	private void OnButtonExit()
@@ -32,28 +50,11 @@ public class UIGame : MonoBehaviour
 	private void OnButtonSettings()
 	{
 		SceneManager.LoadScene("Settings", LoadSceneMode.Additive);
-
-		root.Q<Button>("button-exit").SetEnabled(false);
-		root.Q<Button>("button-settings").SetEnabled(false);
-		root.Q<Button>("button-restart").SetEnabled(false);
-
-		foreach(DraggableItem item in FindObjectsOfType<DraggableItem>()) {
-			item.canDrag = false;
-		}
+		EnableUI(false);
 	}
 
 	private void OnButtonRestart()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-	}
-
-	private void OnSceneUnloaded(Scene unloadedScene) {
-		root.Q<Button>("button-exit").SetEnabled(true);
-		root.Q<Button>("button-settings").SetEnabled(true);
-		root.Q<Button>("button-restart").SetEnabled(true);
-
-		foreach (DraggableItem item in FindObjectsOfType<DraggableItem>()) {
-			item.canDrag = true;
-		}
 	}
 }
