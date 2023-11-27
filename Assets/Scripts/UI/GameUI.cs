76cae5244d847d3fc2763c7315791517f4cb4095
javics2002 <49459590+javics2002@users.noreply.github.com>
2023-11-27 21:56:42 +0100

    Menú de settings arreglado
    Ahora no se puede interactuar con el juego mientras estén los ajustes abiertos, y tampoco se pueden abrir multiples menús de configuración

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -1,17 +1,27 @@
-﻿using UnityEngine;
+﻿using System.Xml.Linq;
+
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 [RequireComponent(typeof(UIDocument))]
 public class UIGame : MonoBehaviour
 {
+	private VisualElement root;
+
 	public void Start()
 	{
-		var root = GetComponent<UIDocument>().rootVisualElement;
+		root = GetComponent<UIDocument>().rootVisualElement;
 
 		root.Q<Button>("button-exit").clicked += OnButtonExit;
 		root.Q<Button>("button-settings").clicked += OnButtonSettings;
 		root.Q<Button>("button-restart").clicked += OnButtonRestart;
+
+		SceneManager.sceneUnloaded += OnSceneUnloaded;
+	}
+
+	private void OnDestroy() {
+		SceneManager.sceneUnloaded -= OnSceneUnloaded;
 	}
 
 	private void OnButtonExit()
@@ -22,10 +32,28 @@ public class UIGame : MonoBehaviour
 	private void OnButtonSettings()
 	{
 		SceneManager.LoadScene("Settings", LoadSceneMode.Additive);
+
+		root.Q<Button>("button-exit").SetEnabled(false);
+		root.Q<Button>("button-settings").SetEnabled(false);
+		root.Q<Button>("button-restart").SetEnabled(false);
+
+		foreach(DraggableItem item in FindObjectsOfType<DraggableItem>()) {
+			item.canDrag = false;
+		}
 	}
 
 	private void OnButtonRestart()
 	{
 		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
+
+	private void OnSceneUnloaded(Scene unloadedScene) {
+		root.Q<Button>("button-exit").SetEnabled(true);
+		root.Q<Button>("button-settings").SetEnabled(true);
+		root.Q<Button>("button-restart").SetEnabled(true);
+
+		foreach (DraggableItem item in FindObjectsOfType<DraggableItem>()) {
+			item.canDrag = true;
+		}
 	}
 }
